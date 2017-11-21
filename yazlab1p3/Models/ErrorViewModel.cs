3156525bb61b511c3fcbda230569e09afcb3b272
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-11-22 00:49:54 +0300

    Add project files.

---
+++ yazlab1p3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace yazlab1p3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
